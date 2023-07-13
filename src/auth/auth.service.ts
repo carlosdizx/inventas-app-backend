236da4c8fd54a2b192b13205b449e0fd4feb9345
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-07-13 17:08:42 -0500

    :sparkles: create dto and complete method for register user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import User from './entities/user.entity';
 import { Repository } from 'typeorm';
+import CreateUserDto from './dto/create.user.dto';
 
 @Injectable()
 export default class AuthService {
   constructor(
     @InjectRepository(User) private readonly repository: Repository<User>,
   ) {}
+
+  public registerUser = async (createUserDto: CreateUserDto) => {
+    try {
+      const user = this.repository.create({
+        ...createUserDto,
+      });
+      await this.repository.save(user);
+      delete user.password;
+      return user;
+    } catch (error) {
+      console.log('Failed to register user');
+    }
+  };
 }
