17665c6dfe7b605a24d57380d1ce6663954189ff
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-07-13 18:09:14 -0500

    :sparkles: create new util service for encrypt password and validate compare

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import User from './entities/user.entity';
 import { Repository } from 'typeorm';
 import CreateUserDto from './dto/create.user.dto';
 import ErrorHandlerService from '../common/utils/error.handler.service';
+import EncryptService from '../common/utils/encrypt.service';
 
 const nameService = 'AuthService';
 @Injectable()
@@ -11,12 +12,16 @@ export default class AuthService {
   constructor(
     @InjectRepository(User) private readonly repository: Repository<User>,
     private readonly errorHandlerService: ErrorHandlerService,
+    private readonly encryptService: EncryptService,
   ) {}
 
   public registerUser = async (createUserDto: CreateUserDto) => {
     try {
       const user = this.repository.create({
         ...createUserDto,
+        password: await this.encryptService.encryptPassword(
+          createUserDto.password,
+        ),
       });
       await this.repository.save(user);
       delete user.password;
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import ErrorHandlerService from './utils/error.handler.service';
+import EncryptService from './utils/encrypt.service';
 
 @Global()
 @Module({
-  providers: [ErrorHandlerService],
-  exports: [ErrorHandlerService],
+  providers: [ErrorHandlerService, EncryptService],
+  exports: [ErrorHandlerService, EncryptService],
 })
 export class CommonModule {}
